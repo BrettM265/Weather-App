7bcff25cc909c700bd03b0a29915ae24f4b22dfc
BrettM265 <78045071+BrettM265@users.noreply.github.com>
2022-01-23 15:44:49 -0600

    Update java.js

--- java.js
+++ java.js
@@ -6,36 +6,77 @@ const searchBar = document.getElementById("searchBar").addEventListener("input",
 var baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 var keyCode = "&units=imperial&appid=947b7e1e972d91d49f8eb5b9e62a4a65";
 let url = baseURL + search + keyCode;
+let container = document.getElementsByClassName("container");
 
-
-   fetch(url)
+  fetch(url)
    .then(response => {
      return response.json();
    })
 
-   .then(data => {
-     console.log(data);
-   displayResults(data)
+  .then(data => {
+    console.log(data);
+    displayResults(data);
    
    
    
-   function displayResults(data) {
-   let location = document.querySelector('#location')
-   location.innerText = `${data.name}, ${data.sys.country}`;
+    function displayResults(data) {
+      let locationStart = document.querySelector('#location-start')
+      locationStart.innerText = `${data.name}, ${data.sys.country}`;
 
-   let temp = document.querySelector('#temp')
-   temp.innerText = `Temperature: ${Math.round(data.main.temp)} °F`;
+      let tempNum = document.querySelector('#temp-num')
+      tempNum.innerText = `${Math.round(data.main.temp)} °F`;
 
-   let feels = document.querySelector('#feels')
-   feels.innerText = `Feels like: ${Math.round(data.main.feels_like)} °F`;
+      let descTop = document.querySelector('#desc-top')
+      descTop.innerText = `${data.weather[0].description}`
 
-   let wind = document.querySelector('#wind')
-   wind.innerText = `Wind: ${Math.round(data.wind.speed)} Mph Winds`;
+      let location = document.querySelector('#location')
+      location.innerText = `${data.name}, ${data.sys.country}`;
 
-   let desc = document.querySelector('#desc')
-   desc.innerText = `Description: ${data.weather[0].description}`
-   }
+      let temp = document.querySelector('#temp')
+      temp.innerText = `- Temperature: ${Math.round(data.main.temp)} °F`;
+
+      let feels = document.querySelector('#feels')
+      feels.innerText = `- Feels like: ${Math.round(data.main.feels_like)} °F`;
+
+      let min = document.querySelector('#min')
+      min.innerText = `- Min ${Math.round(data.main.temp_min)} °F`;
+
+      let max = document.querySelector('#max')
+      max.innerText = `- Max ${Math.round(data.main.temp_max)} °F`;
+
+      let wind = document.querySelector('#wind')
+      wind.innerText = `- ${Math.round(data.wind.speed)} Mph Winds`;
+
+      let desc = document.querySelector('#desc')
+      desc.innerText = `- ${data.weather[0].description}`
+
+      if(data.weather[0].description.includes("snow") == true){
+        document.getElementById("main-container").style.background = "url(weather-icon/snowy.jpg)";
+      }
+      
+      if(data.weather[0].description.includes("clouds") == true){
+        document.getElementById("main-container").style.background = "url(weather-icon/overcast2.jpg)";
+      }
+      if(data.weather[0].description.includes("few clouds") == true){
+        document.getElementById("main-container").style.background = "url(weather-icon/clear-day.jpg)";
+      }
+      if(data.weather[0].description.includes("clear") == true){
+        document.getElementById("main-container").style.background = "url(weather-icon/clear-day.jpg)";
+      }
+      if(data.weather[0].description.includes("rain") == true){
+        document.getElementById("main-container").style.background = "url(weather-icon/rain-img.jpg)";
+      }
+
+    }
   })
 })
 
 
+// Image Credit Popup
+function popUp(){
+  document.getElementById("credit-popup").style.visibility = "visible";
+}
+
+function closePop(){
+  document.getElementById("credit-popup").style.visibility = "hidden";
+}
